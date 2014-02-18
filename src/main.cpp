116d0c3f6343da01af267ca3af898e2dd2ec5e36
Chad Hatcher <hackenslacker@gmail.com>
2014-02-18 13:48:47 -0800

    [main] Implement usage of text display widget.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,26 @@
+
 #include <iostream>
+#include <sstream>
 #include "sdl-manager.h"
 
 static bool running = true;
 static SliderReference slider = 0;
+static TextDisplayReference textDisplay = 0;
+
+inline std::string toString(double x)
+{
+  std::ostringstream o;
+  o << x;
+  return o.str();
+}
 
 void sliderCallback(SDL_Event & event)
 {
+	std::string text;
+	text += toString(100 * slider->getValue());
+	text += "%";
 	std::cout << "Slider set: " << slider->getValue() << std::endl;
+	textDisplay->setText(text);
 }
 
 void stopRunning(SDL_Event & event)
@@ -26,6 +40,7 @@ int main(int argc, char **argv)
 	sdl.subscribeToEvent(stopRunning, SDL_KEYDOWN, '\e');
 	sdl.createButton(stopRunning, 0, "Quit", 16, 552, 128, 32);
 	slider = sdl.createSlider(sliderCallback, 0, 16, 200);
+	textDisplay = sdl.createTextDisplay("", 16+256+16, 200);
 
 	// While application is running
 	while(running)
