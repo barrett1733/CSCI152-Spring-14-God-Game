a588cdffc9ed880c7e813929cf1a42f96d70ff76
Chad Hatcher <hackenslacker@gmail.com>
2014-02-18 13:47:00 -0800

    [SDL Text Display Widget] Implement text display widget.

---
+++ src/sdl-text-display.cpp
@@ -0,0 +1,42 @@
+
+#include "sdl-text-display.h"
+
+SdlTextDisplay::SdlTextDisplay(SDL_Surface * surface_arg, SDL_Rect & rect, std::string text_arg) :
+	SdlWidgetBase(surface_arg, rect, 0)
+{
+	text = text_arg;
+	font = TTF_OpenFont( "res/arial.ttf", 16 );
+	color.r = 0;
+	color.g = 0;
+	color.b = 0;
+	color.a = 255;
+
+	buildSurface();
+}
+
+SdlTextDisplay::~SdlTextDisplay()
+{
+	TTF_CloseFont(font);
+}
+
+void SdlTextDisplay::setText(std::string text_arg)
+{
+	text = text_arg;
+	buildSurface();
+}
+
+void SdlTextDisplay::buildSurface()
+{
+	// Clear the existing surface.
+	SDL_FillRect(surface, NULL, SDL_MapRGBA(surface->format, 0, 0, 0, 0));
+
+	if(text.length())
+	{
+		// Create a new text surface.
+		SDL_Surface * textSurface = TTF_RenderText_Solid(font, text.c_str(), color);
+
+		// Blit the text surface onto the background and free the text surface
+		SDL_BlitSurface(textSurface, NULL, surface, NULL);
+		SDL_FreeSurface(textSurface);
+	}
+}
---
+++ src/sdl-text-display.h
@@ -0,0 +1,27 @@
+
+#ifndef SDL_TEXT_DISPLAY_H_
+#define SDL_TEXT_DISPLAY_H_
+
+#include "sdl-widget.h"
+#include <SDL2_ttf/SDL_ttf.h>
+
+#include <string>
+
+class SdlTextDisplay : public SdlWidgetBase
+{
+	TTF_Font * font;
+	SDL_Color color;
+	std::string text;
+
+	void buildSurface();
+
+public:
+	SdlTextDisplay(SDL_Surface * surface_arg, SDL_Rect & rect, std::string text_arg);
+	~SdlTextDisplay();
+
+	void setText(std::string text_arg);
+};
+
+typedef SdlTextDisplay * TextDisplayReference;
+
+#endif
