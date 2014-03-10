164cbe564fc30e283bcfb0547bc0320b80a1ee32
Allen Mills <allen_mills@mail.fresnostate.edu>
2014-03-10 16:55:47 -0700

    [Miracles] Initial commit.

--- src/Miracle.h
+++ src/Miracle.h
@@ -2,44 +2,33 @@
  * Miracles: An action or actions that are defined by the player.
  * Ex: Summon:Cow, AOE:Heal
  *
- * Miracle Manager
- *		Takes input data and returns an Entity:Miracle
  */
  #include <map>
- #include <string>
  #include "entity.h"
  #include "entity.cpp"
- #include "config.cpp"
  
- 
-typedef std::map<EntityType, std::int> MiracleMap;
+ struct Pair{
+	std::string name;
+	int cost;
+ }
+
+ // Entity class for miracle to enable manager to interact with entities
+class MiracleEntity:Entity{
+	//	static int AOE=0;
+	//	virtual Action()=0;
+}
+
+typedef std::map<std::string, std::int> MiracleMap;
+typedef std::pair<std::string, std::int> MiraclePair;
 
 //miracle manager. go between for player and map
-class MiracleManager:config{
-	// data
-private:
+class MiracleManager:Config{
 	MiracleMap miracleList;
-	bool setProperty(std::string property, int value);
+	ResourceManager *resources;
 	
-public:
-	MiracleManager(std::string file); // constructor
-	int getCost(EntityType e);
-	Entity* createMiracle(EntityType, Position);
+	Miracle(std::string fileName,ResourceManager *);
+	int getCost(std::string);
+	int getET_Name(std::string);
+	bool castMiracle(std::string, Callback input);
+	void setResourceManager(ResourceManager *);
 }
-
-MiracleManager::MiracleManager(std::string file){
-	// All we need is to load the config file.
-	load(file);
-}
-int MiracleManager::getCost(EntityType e){return miracleList[e];}
-Entity* MiracleManager::createMiracle(EntityType et, Position p, Faction f){
-	// Will make an entity based on incoming data.
-	return (new Entity(et,100,p,f));
-}
-
-////  Config Method
-bool MiracleManager::setProperty(std::string property, int value){
-	miracleList[property]=value;
-}
-
-
