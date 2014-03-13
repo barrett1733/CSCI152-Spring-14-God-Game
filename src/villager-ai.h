206c7076c7100332fa45002572252d2cbdc48229
Chad Hatcher <chatcher@users.noreply.github.com>
2014-03-13 12:10:41 -0700

    [Village AI] Add comment about Triangle Values.

--- src/villager-ai.h
+++ src/villager-ai.h
@@ -1,4 +1,3 @@
-
 #include "resource-manager.h"
 
 class VillagerAI : public ResourceManager
@@ -10,6 +9,14 @@ private:
 	bool running;
 	void balanceJobs();
 	void checkResource(int);
+	
+	// VillageAI should be agnostic as to HOW the triangle values are set.
+	// Instead, it should accept the values as in:
+	//void setTriangleStats(double a, double b, double c);
+	// Then again, I'm not sure the village AI needs to know about the triangle at all.
+	// I think it's mostly for the job management.
+	// The Job Manager will set the priority on tasks base on the triangle,
+	// and villages will pick up tasks base on the task priority and their own preference.
 	void getTriangleStats();
 
 public:
