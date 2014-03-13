f907e4a144e9d6c1db8e6c8cb5379a444a10879a
Chad Hatcher <chatcher@users.noreply.github.com>
2014-03-13 12:01:21 -0700

    [Res Manager] Add comment about #defining the resource count.

--- src/resource-manager.h
+++ src/resource-manager.h
@@ -1,4 +1,3 @@
-
 #define RESOURCE_TYPE_MAX 4
 
 enum ResourceType
@@ -8,6 +7,14 @@ enum ResourceType
 	RS_FOOD,
 	RS_STONE,
 	RS_IRON
+	
+	// Making the count part of the enum allows you
+	// to add or remove enumerations without having to make two updates
+	// (one to add the enumerations and one to update the count).
+	// This only works if your enum's int values start at zero
+	// and are automatically incremented (as is the case here).
+	// I.e., move NUM_RESOURCETYPE from below as a define, to here:
+	//NUM_RESOURCETYPES
 };
 
 #define NUM_RESOURCETYPES 5
