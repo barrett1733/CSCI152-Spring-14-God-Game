ea3a61298d20fbd0a5ed515c14bb04b8d4a24461
Chad Hatcher <chad@onfarmsystems.com>
2014-03-15 15:19:04 -0700

    [SDL Map View] Remove commented code. Improve performance.
    The map image size was five times what it should have been.

--- src/sdl/sdl-map-view.cpp
+++ src/sdl/sdl-map-view.cpp
@@ -1,16 +1,6 @@
 
 #include "sdl-map-view.h"
 
-// SdlMapView::SdlMapView(SDL_Surface * surface_arg, SDL_Rect & rect, void (*callback_arg)(SDL_Event & event)) :
-// 	SdlWidget(surface_arg, rect, callback_arg)
-// {}
-
-// SdlMapView::SdlMapView(const char * text, SDL_Rect & rect, void (*callback_arg)(SDL_Event & event)) :
-// 	SdlWidget(0, rect, callback_arg)
-// {
-// 	surface = createBackground(rect);
-// }
-
 SdlMapView::SdlMapView(int xPos, int yPos, int width, int height) :
 	SdlWidget()
 {
@@ -22,29 +12,13 @@ SdlMapView::SdlMapView(int xPos, int yPos, int width, int height) :
 SdlMapView::~SdlMapView()
 {}
 
-void SdlMapView::handleEvent(SDL_Event & event)
-{
-	updateState(event);
-
-	if(state == WS_ON)
-	{
-		if(callback)
-			callback(event, this);
-		state = WS_OFF;
-	}
-
-	// Update button appearance for button state
-	clipping.y = boundingBox.h * state;
-}
-
 ImageReference SdlMapView::createBackground(SDL_Rect & rect)
 {
 	int width = rect.w;
 	int height = rect.h;
 
-	ImageReference image = sdlUtility.createSurface(width, 5 * height);
+	ImageReference image = sdlUtility.createSurface(width, height);
 	SDL_FillRect(image, NULL, sdlUtility.getColor(image,C_BEIGE));
-
 
 	return image;
 }
--- src/sdl/sdl-map-view.h
+++ src/sdl/sdl-map-view.h
@@ -9,12 +9,8 @@ class SdlMapView : public SdlWidget
 	ImageReference createBackground(SDL_Rect & rect);
 
 public:
-	// SdlMapView(SDL_Surface * surface, SDL_Rect & rect, void (*callback_arg)(SDL_Event & event));
-	// SdlMapView(const char * text, SDL_Rect & rect, void (*)(SDL_Event&));
 	SdlMapView(int xPos, int yPos, int width, int height);
 	~SdlMapView();
-
-	void handleEvent(SDL_Event & event);
 };
 
 typedef SdlMapView * MapViewReference;
