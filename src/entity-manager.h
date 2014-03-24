dd8684b4cb12a9e496713d315c4ada8dcd11ba9f
Allen Mills <allen_mills@mail.fresnostate.edu>
2014-03-24 12:27:33 -0700

    [EntityManager] Added a few more methods and lists to Entity Manager header. As I was looking at this, it occurs to me; we need a way of taking an SDL-Entity or a normal Entity and finding the other. We have no interactions between these two and I don't yet know how to take one and implicitly create the other.

--- src/entity-manager.h
+++ src/entity-manager.h
@@ -16,9 +16,20 @@ struct EM_Record
 class EntityManager
 {
 	std::vector<WidgetReference> widgetList;
-
+	std::vector<Entity*> entityList; // list of all current entities on map
+	// these lists are sub-catagories of the entitylist, should still be the same reference //
+	std::vector<Entity*> villagerList;
+	std::vector<Entity*> peacefulMobList;
+	std::vector<Entity*> enemyList;
+	std::vector<Entity*> buildingList;
+	
 public:
 	void createEntity();
+	void deleteEntity(); // removes entity from all applicable lists
+	
+	void getEntityType(); 
+	// needs a return of some kind, this will define what lists 
+	//   the entity is added to or removed from.
 
 	void update();
 	void show();
