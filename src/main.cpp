94c51aa0250963830e97ed4cbe11f18bca45b4bd
Chad Hatcher <hackenslacker@gmail.com>
2014-03-27 13:05:46 -0700

    [Main] Separate the game loop into three sections: Menu, Loading, Playing.

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,17 @@ int main(int argc, char **argv)
 	GameManager game;
 	GameMode gameMode = GM_ERROR;
 
-	// While application is running
 	std::cout << "Starting Game Loop" << std::endl;
+	while(game.mode() == GM_MENU)
+		sdl.update();
+
+	if(game.mode() == GM_PLAYING)
+	{
+		std::cout << "Setting up new game." << std::endl;
+		// do world gen, set up new game, etc.
+	}
+
+	std::cout << "Continuing Game Loop" << std::endl;
 	while(( gameMode = game.mode() ) != GM_QUITTING)
 	{
 		sdl.update(); //  this should be the last call, because it will consume the rest of the frame's time.
