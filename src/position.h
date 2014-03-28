65508fd829a5a7a44a9ebe0db3866f1127e8a3e3
Chad Hatcher <chad@onfarmsystems.com>
2014-03-27 23:43:30 -0700

    [Position] Add constructors. Change data type from double to int (again).

--- src/position.h
+++ src/position.h
@@ -6,12 +6,24 @@
 #define POSITION_H_
 
 #include <math.h>
+#include <iostream>
 
 class Position
 {
 public:
-	double x;
-	double y;
+	Position() : x(0), y(0) {}
+	Position(int x, int y) : x(x), y(y) {}
+
+	const Position& operator= (const Position & position)
+	{
+		x = position.x;
+		y = position.y;
+
+		return *this;
+	}
+
+	int x;
+	int y;
 
 	double distance(Position & p)
 	{
@@ -21,6 +33,13 @@ public:
 	double direction(Position & p)
 	{
 		return atan(y/x) + (x < 0 ? 3.14159265359 : 0);
+	}
+
+
+	friend std::ostream& operator<< (std::ostream & os, const Position & position)
+	{
+		os << "(" << position.x << ", " << position.y << ")";
+		return os;
 	}
 };
 
