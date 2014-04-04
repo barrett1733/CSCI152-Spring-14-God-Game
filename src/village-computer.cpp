9ead2d325ef81bf45b4639838e48c700482e4417
barrett1733 <barrett1733@sbcglobal.net>
2014-04-03 20:44:28 -0700

    [AI] Copied functions from old ComputerAI to VillageComputer. Deleted computer-ai.

--- src/village-computer.cpp
+++ src/village-computer.cpp
@@ -1,8 +1,10 @@
 #include "village-computer.h"
 
 
-VillageComputer::VillageComputer(Faction faction) : Village(faction)
+VillageComputer::VillageComputer(Faction faction, Difficulty difficulty) : Village(faction), difficulty(difficulty)
 {
+	for (int i = 0; i<JT_COUNT; i++)
+		jobsArray[i];
 }
 
 void VillageComputer::run()
@@ -12,4 +14,19 @@ void VillageComputer::run()
 	// Determine miracles
 	// Determine Triangle balance.
 	// jobManager.setBalance(militaryBalance, buildBalance, gatherBalance);
+}
+
+bool VillageComputer::requestVillager(JobType job, int amount)
+{
+	if (amount <= villagers)
+	{
+		jobsArray[job] += amount;
+		int remainder = amount % 2;
+		for (int i = 0; i<JT_COUNT - 1; i++)
+		if (i != job)
+			jobsArray[i] += amount / 2;
+		jobsArray[JT_COUNT - 2] += remainder;
+		return true;
+	}
+	else return false;
 }
--- src/village-computer.h
+++ src/village-computer.h
@@ -1,10 +1,24 @@
 #include "village.h"
 #pragma once
 
+enum Difficulty
+{
+	EASY = 1,
+	NORMAL,
+	HARD
+};
+
 class VillageComputer : public Village
 {
+private:
+	Difficulty difficulty;
+	enum JobType { GATHER, MILITARY, BUILD, WORSHIP, JT_COUNT };
+	int villagers;
+	double jobsArray[JT_COUNT];
+	bool requestVillager(JobType, int);
+
 public:
-	VillageComputer(Faction);
+	VillageComputer(Faction, Difficulty);
 	void run();
 };
 
