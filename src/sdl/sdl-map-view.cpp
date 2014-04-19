73ae9ff539a019391bdff72753b625f58353b410
Chad Hatcher <chad@onfarmsystems.com>
2014-04-18 23:31:41 -0700

    [Map View] Fix issue with tiles not being cleared.

--- src/sdl/sdl-map-view.cpp
+++ src/sdl/sdl-map-view.cpp
@@ -33,7 +33,8 @@ ImageReference SdlMapView::createBackground(SDL_Rect & rect)
 
 void SdlMapView::clear(SDL_Rect & rect)
 {
-	SDL_BlitSurface(clearSurface, NULL, surface, &rect);
+	SDL_Rect clip = sdlUtility.createRect(0, 0, rect.w - 1, rect.h - 1);
+	SDL_BlitSurface(clearSurface, &clip, surface, &rect);
 
 	if(texture) SDL_DestroyTexture(texture);
 	texture = 0;
