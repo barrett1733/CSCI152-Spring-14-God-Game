d622ddcf499ddbbcfa6cc066ce617c723c673a82
Steven Barrett <barrett1733@sbcglobal.net>
2014-04-21 15:54:48 -0700

    [Creature] Updated creature ai. Fixed compile errors.

--- src/creature.cpp
+++ src/creature.cpp
@@ -1,23 +1,42 @@
 #include "creature.h"
 
-
-Creature::Creature()
-{ }
-Creature::Creature(Faction faction) : faction(faction)
+Creature::Creature(Faction faction) :
+faction(faction)
 { }
 
-void Creature::decideRun()
+void Creature::importEntity(EntityReference entity)
+{
+	Faction faction = entity->getFaction();
+	EntityGroup group = entity->getGroup();
+	if (faction == this->faction)
+	{
+		if (group == EG_DOMESTIC)
+			domesticList.push_back((MobileEntityReference)entity);
+
+		else if (group == EG_PASSIVE)
+			passiveList.push_back((MobileEntityReference)entity);
+
+		else if (group == EG_HOSTILE)
+			hostileList.push_back((MobileEntityReference)entity);
+	}
+}
+
+void Creature::attack()
 {
 
 }
 
-void Creature::decideWalk()
+void Creature::flee()
 {
 
 }
 
-void Creature::run()
+void Creature::wander(CreatureReference creature)
 {
-	decideRun();
-	decideWalk();
+	
+}
+void Creature::update()
+{
+	for (std::iterator<Creature> *iter = passiveList.begin(); iter != passiveList.end(); ++iter)
+		wander((MobileEntityReference) iter);
 }
--- src/creature.h
+++ src/creature.h
@@ -1,20 +1,29 @@
+#include "entity.h"
 #include "job-manager.h"
+#include <vector>
+
 #pragma once
 
 class Creature
 {
-private:
-	JobManager jobManager;
-	void decideRun();
-	void decideWalk();
-
-protected:
 	Faction faction;
 
+	std::vector<MobileEntityReference> domesticList; // list of domestic things (cow)
+	std::vector<MobileEntityReference> passiveList; // list of passive things (deer)
+	std::vector<MobileEntityReference> hostileList; // list of hostile things (orges, wolves)
+
 public:
-	Creature();
 	Creature(Faction);
-	void run();
+
+	void importEntity(EntityReference);
+
+	void wander();
+	void attack();
+	void flee();
+
+	void update();
+
+	Faction getFaction() { return faction; }
 };
 
-typedef Creature * PlayerReference;
+typedef Creature * CreatureReference;
