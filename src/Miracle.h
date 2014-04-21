52dcd63bb6fddd8580f65c3d795b23fa890efe16
Allen Mills <allen_mills@mail.fresnostate.edu>
2014-04-21 16:40:17 -0700

    [Miracle] I believe that I'm done here. Yay.

--- src/Miracle.h
+++ src/Miracle.h
@@ -2,20 +2,13 @@
  * Miracles: An action or actions that are defined by the player.
  * Ex: Summon:Cow, AOE:Heal
  *
- * [Miracle] -> (Entity miracle,int cost)
- * [Somthing] -> (Entity miracle)-> [Miracle Manager |? resource.faith-miracle.cost >= 0 ] -> (Entity Miracle) -> [Entity Manager]
- * [Miracle Manager]
- * 	-Map:MiracleCost, EntityType // this is our unique id between entities
- * 	-ResourceManager* // who are we taking resources from
- * 	-EntityManager* // who are we sending a miracle to.
- *
- *
+ * Miracle Manager
+ *		Takes input data and returns an Entity:Miracle
  */
  #include <map>
  #include <string>
  #include "entity.h"
  #include "entity.cpp"
- #include "entity-manager.cpp"
  #include "config.cpp"
  
  
@@ -26,20 +19,22 @@ class MiracleManager:config{
 	// data
 private:
 	MiracleMap miracleList;
-	
-	ResourceManager *resources;
-	EntityManager *entities;
-	
-	//// Methods
-	// set up
-	MiracleManager(std::string file){load(file);} // constructor
-	
-	int getCost(EntityType e){return miracleList[e];}
-	Entity* castMiracle(EntityType, Position); // adds Entity to EntityManager if resource manager has enough faith
-	
-	// Inherited methods from config
-	// void load(string file); 
 	bool setProperty(std::string property, int value);
+	
+public:
+	MiracleManager(std::string file); // constructor
+	int getCost(EntityType e);
+	Entity* createMiracle(EntityType, Position);
+}
+
+MiracleManager::MiracleManager(std::string file){
+	// All we need is to load the config file.
+	load(file);
+}
+int MiracleManager::getCost(EntityType e){return miracleList[e];}
+Entity* MiracleManager::createMiracle(EntityType et, Position p, Faction f){
+	// Will make an entity based on incoming data.
+	return (new Entity(et,100,p,f));
 }
 
 ////  Config Method
