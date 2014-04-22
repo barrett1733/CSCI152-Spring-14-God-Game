a259e5b366293eda6f9dc3a121025c48afe83f39
Chad Hatcher <chad@onfarmsystems.com>
2014-04-21 20:35:46 -0700

    (+) [SDL Widget Container] Implement a Widget Container.
    Widget Container inherits from both SdlWidget (does it need to?) and Config.
    
    It requires a callback map and a config file.

---
+++ src/sdl/sdl-widget-container.h
@@ -0,0 +1,37 @@
+
+#ifndef SDL_WIDGET_CONTAINER_H_
+#define SDL_WIDGET_CONTAINER_H_
+
+#include <vector>
+#include <map>
+
+#include "sdl-widget.h"
+#include "../config.h"
+
+class SdlWidgetContainer : public SdlWidget, public Config
+{
+	std::vector<WidgetReference> widgetList;
+	std::map<std::string, void (*)(SDL_Event&, WidgetReference)> callbackMap;
+
+	bool visible;
+
+	std::string callbackName;
+	std::string buttonLabel;
+	SDL_Rect rect;
+	int buttonConfig;
+
+public:
+	SdlWidgetContainer(std::map<std::string, void (*)(SDL_Event&, WidgetReference)> callbackMap, const char * loadFileName);
+
+	void show();
+	void hide();
+
+	// From Config
+	bool setProperty(std::string property, std::string value);
+	bool setProperty(std::string property, int value);
+	bool setProperty(std::string property, int value1, int value2);
+};
+
+#endif
+
+typedef SdlWidgetContainer * WidgetContainerReference;
