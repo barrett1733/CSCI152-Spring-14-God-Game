5e901c437ed6fa3d66f409c7112573eed95d37cf
Steven Barrett <barrett1733@sbcglobal.net>
2014-04-24 02:56:23 -0700

    [Village] Added stuff from AI script. Removed hasGodLogic() and runGodLogic() for now.

--- src/village.h
+++ src/village.h
@@ -17,22 +17,34 @@ class Village
 	std::vector<MobileEntityReference> domesticList; // list of domestic things (cows)
 	std::vector<EntityReference> buildingList; // list of buildings
 	// Should resources be 'discovered' instead of all known immediately?
+	//
+	// TODO: Villager creation every 30 secs
+	//
+	bool villageStarted;
+	int populationCap;
+	bool setBeginningPopCap;
 
-	void balanceJobs();
-	void createBuildings();
-	void needsDefending();
+	void villageStart();
+
+	void decideAction();
+
+	int getBuildingCount(EntityType);
+	void setPopulationCap();
+
+	void buildHouse();
+	void buildMasonry();
+	void buildMill();
+	void buildSmith();
+	void buildArmory();
+	void buildFoundary();
 
 public:
 	Village(Faction);
-	void run();
 
 	void importEntity(EntityReference);
 
-	bool hasGodLogic();
-	void runGodLogic();
 	void update();
 
-	void buildHouse();
 
 	Faction getFaction() { return faction; }
 };
