17918cf945dbf28f52bca5b213ca19f2630a8d94
Chad Hatcher <chad@onfarmsystems.com>
2014-04-26 15:10:59 -0700

    [Miracles] Fix spacing, line breaks, missing semicolons.

--- src/miracle-manager.h
+++ src/miracle-manager.h
@@ -6,29 +6,32 @@
  #include <map>
  #include "entity.h"
  #include "entity.cpp"
- 
- struct Pair{
+
+ struct Pair
+ {
 	std::string name;
 	int cost;
- }
+ };
 
  // Entity class for miracle to enable manager to interact with entities
-class MiracleEntity:Entity{
+class MiracleEntity : public Entity
+{
 	//	static int AOE=0;
 	//	virtual Action()=0;
-}
+};
 
 typedef std::map<std::string, std::int> MiracleMap;
 typedef std::pair<std::string, std::int> MiraclePair;
 
 //miracle manager. go between for player and map
-class MiracleManager:Config{
+class MiracleManager : public Config
+{
 	MiracleMap miracleList;
 	ResourceManager *resources;
-	
+
 	Miracle(std::string fileName,ResourceManager *);
 	int getCost(std::string);
 	int getET_Name(std::string);
 	bool castMiracle(std::string, Callback input);
 	void setResourceManager(ResourceManager *);
-}
+};
