852c55b4d0fb1df64a8578f4c7a321c630374ad2
Allen Mills <allen_mills@mail.fresnostate.edu>
2014-05-03 17:13:33 -0700

    [Miracles] Done setting up miracle-entity:update, at least I think. This will need some testing to make sure there are no compile issues.

--- src/miracle-entity.cpp
+++ src/miracle-entity.cpp
@@ -5,23 +5,40 @@ miracle-entity(EntityType, int health, Position, Faction):Entity(EntityType, int
 miracle-entity(const Entity&):Entity(const Entity&){}
 
 void update(std::vector<Entity*>& entityList, ObstructionMapReference obstructionMap){
+	std::vector<Entity*> affectedEntitiesList;
 	if(EntityType==ET_MIRACLE_HEAL){
-	
+		affectedEntitiesList=getEntitiesWithin(50.0, this->getPosition(),entityList);
+		for(int i=0; i<affectedEntitiesList.size(); i++){
+			affectedEntitiesList[i].setCurrentHealth(affectedEntitiesList[i].getCurrentHealth()+20);
+		}
 	}
 	else if(EntityType==ET_MIRACLE_SUMMONCOW){
-	
+		entityList.pushBack(new Entity(ET_COW, 100, this->getPosition(), this->getFaction()));
 	}
 	else if(EntityType==ET_MIRACLE_STATBOOST){
-	
+		affectedEntitiesList=getEntitiesWithin(75.0, this->getPosition(),entityList);
+		for(int i=0; i<affectedEntitiesList.size(); i++){
+			affectedEntitiesList[i].setMaxHealth(affectedEntitiesList[i].getMaxHealth()*2);
+			affectedEntitiesList[i].setCurrentHealth(affectedEntitiesList[i].getMaxHealth());
+		}
 	}
 	else if(EntityType==ET_MIRACLE_LIGTNING){
-	
+		affectedEntitiesList=getEntitiesWithin(25.0, this->getPosition(),entityList);
+		for(int i=0; i<affectedEntitiesList.size(); i++){
+			affectedEntitiesList[i].setCurrentHealth(affectedEntitiesList[i].getCurrentHealth()*0.75-20);
+		}
 	}
 	else if(EntityType==ET_MIRACLE_EARTHQUAKE){
-	
+		affectedEntitiesList=getEntitiesWithin(100.0, this->getPosition(),entityList);
+		for(int i=0; i<affectedEntitiesList.size(); i++){
+			affectedEntitiesList[i].setCurrentHealth(affectedEntitiesList[i].getCurrentHealth()*0.50-20);
+		}
 	}
 	else if(EntityType==ET_MIRACLE_METEOR){
-	
+		affectedEntitiesList=getEntitiesWithin(80.0, this->getPosition(),entityList);
+		for(int i=0; i<affectedEntitiesList.size(); i++){
+			affectedEntitiesList[i].setCurrentHealth(0);
+		}
 	}
 	else{
 	
