a837583f4005150ef36041afd3c72e2dec196f16
Steven Barrett <barrett1733@sbcglobal.net>
2014-05-06 14:55:56 -0700

    [ResourceManager] Removed resourcePool pointer.

--- src/resource-manager.cpp
+++ src/resource-manager.cpp
@@ -1,35 +1,12 @@
 #include "resource-manager.h"
 // Resource Manager with no pool
-ResourceManager::ResourceManager()
-{
-	ptr_resourcePool = 0;
-}
-
-// Creates ResourceManager that uses passed ResourcePool
-ResourceManager::ResourceManager(ResourcePool &resourcePool)
-{
-	if(ptr_resourcePool != &resourcePool)
-		ptr_resourcePool = &resourcePool;
-}
-
-ResourceManager::~ResourceManager()
-{
-	ptr_resourcePool = 0;
-}
-
-// Registers a ResourcePool
-void ResourceManager::registerResourcePool(ResourcePool &resourcePool)
-{
-	ptr_resourcePool = &resourcePool;
-}
-
 // Subtracts XXX amount of resources from the ResourcePool
 // Returns true if successful, false if not
 bool ResourceManager::requestResource(ResourceType type, int amount, Faction faction)
 {
-	if(ptr_resourcePool->resourcePool[faction][type] >= amount)
+	if(resourcePool.resourcePool[faction][type] >= amount)
 	{
-		ptr_resourcePool->resourcePool[faction][type] -= amount;
+		resourcePool.resourcePool[faction][type] -= amount;
 		return true;
 	}
 	else
@@ -39,11 +16,11 @@ bool ResourceManager::requestResource(ResourceType type, int amount, Faction fac
 // Adds XXX amount of resources back to the pool
 void ResourceManager::sendResource(ResourceType type, int amount, Faction faction)
 {
-	ptr_resourcePool->resourcePool[faction][type] += amount;
+	resourcePool.resourcePool[faction][type] += amount;
 }
 
 // Get XXX amount of resource
 int ResourceManager::getResourceAmount(ResourceType type, Faction faction)
 {
-	return ptr_resourcePool->resourcePool[faction][type];
+	return resourcePool.resourcePool[faction][type];
 }
